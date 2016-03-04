9fdb4fe345b57b1c2a395d1017ed01c6b1cb4344
Ates Goral <ates.g@myplanet.com>
2016-03-03 20:38:49 -0500

    Pick up additional args, fix arg order

--- index.js
+++ index.js
@@ -10,17 +10,20 @@ module.exports = function (req, options) {
       files: {}
     };
 
-    busboy.on('field', function (name, value, encoding, mimeType) {
+    busboy.on('field', function (name, value, isNameTruncated, isValueTruncated, encoding, mimeType) {
       parts.fields[name] = {
         value: value,
+        isNameTruncated: isNameTruncated,
+        isValueTruncated: isValueTruncated,
         encoding: encoding,
         mimeType: mimeType
       };
     });
 
-    busboy.on('file', function (name, file, encoding, mimeType) {
+    busboy.on('file', function (name, file, filename, encoding, mimeType) {
       parts.files[name] = {
         file: file,
+        filename: filename,
         encoding: encoding,
         mimeType: mimeType
       };
